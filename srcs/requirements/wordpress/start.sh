6091be9c452f632293f7170fd609e650959f35ff
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-08-29 17:12:30 +0300

    users

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,5 +1,12 @@
 #!/user/bin/env bash
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+wp user create dictator dictator@example.com --role=administrator --user_pass=tropico
+wp user create bob bob@example.com --role=author --user_pass=alice
+
 mkdir -p /run/php
 touch /run/php/php7-fpm.pid
 exec php-fpm7.3 --nodaemonize
