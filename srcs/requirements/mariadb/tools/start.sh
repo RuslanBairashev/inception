58e53ce4abfdd0206aff05e322a1303ead64160c
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-08-31 16:00:24 +0300

    mariadb env

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,9 +1,9 @@
 #!/user/bin/env bash
 # Стартуем mysql
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${USER}'@'%' IDENTIFIED BY '${PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 service mysql stop
 #exec bash
