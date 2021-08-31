2591e23ea5edc678b70cce73ecb1b1d4ef2af3a8
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-08-31 14:18:20 +0300

    42.fr

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,17 +1,13 @@
 #!/user/bin/env bash
 
-chmod -R 775 /var/www/html/*
-chown -R www-data /var/www/html/*
-chgrp -R www-data /var/www/html/*
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+##curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 chmod +x ./wp-cli.phar
 #sudo mv ./wp-cli.phar /usr/local/bin/wp
 cp ./wp-cli.phar /usr/local/bin/wp
-wp core install --allow-root --url=localhost --title=43 --admin_user=bobik --admin_email=b@weww.com --path=/var/www/html
-wp user create dictator dictator@example.com --role=administrator --user_pass=tropico --allow-root --path=/var/www/html
-wp user create bob bob@example.com --role=author --user_pass=alice --allow-root --path=/var/www/html
+wp core install --allow-root --url=rmerrie.42.fr --title=inception --admin_user=dictator --admin_password=tropico --admin_email=b@weww.com --path=/var/www/html
+wp user create bob bob@example.com --allow-root --role=author --user_pass=alice --path=/var/www/html
 
 mkdir -p /run/php
 touch /run/php/php7-fpm.pid
