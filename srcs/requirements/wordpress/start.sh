7cfb9527c3661a4b7df37e4c05093a5c872363e8
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-09-04 10:37:51 +0300

    php modules update

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -4,11 +4,9 @@ sed -i -e "s/\${DB_DATABASE}/${DB_DATABASE}/g" /var/www/html/wp-config.php
 sed -i -e "s/\${DB_USER}/${DB_USER}/g" /var/www/html/wp-config.php
 sed -i -e "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /var/www/html/wp-config.php
 sed -i -e "s/\${DB_HOST}/${DB_HOST}/g" /var/www/html/wp-config.php
-##curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 chmod +x ./wp-cli.phar
-#sudo mv ./wp-cli.phar /usr/local/bin/wp
 cp ./wp-cli.phar /usr/local/bin/wp
 sleep 10
 wp core install --allow-root --url=rmerrie.42.fr --title=inception --admin_user=dictator --admin_password=tropico --admin_email=b@weww.com --path=/var/www/html
@@ -17,4 +15,3 @@ wp user create bob bob@example.com --allow-root --role=author --user_pass=alice 
 mkdir -p /run/php
 touch /run/php/php7-fpm.pid
 exec php-fpm7.3 --nodaemonize
-#exec php -S localhost:9000
