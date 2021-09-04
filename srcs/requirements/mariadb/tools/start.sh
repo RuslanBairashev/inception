ed9584b2cf4c9d0a1dc351bc7a75dd8e6fa25606
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-09-04 11:51:07 +0300

    all done

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,22 +1,15 @@
 #! /bin/bash
-echo "[mysqld]" >> /etc/mysql/mariadb.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.cnf
-echo "[client]" >> /etc/mysql/mariadb.cnf
-echo "user=root" >> /etc/mysql/mariadb.cnf
-echo "password=123" >> /etc/mysql/mariadb.cnf
+echo "[client]" >> /etc/mysql/my.cnf
+echo "user=root" >> /etc/mysql/my.cnf
+echo "password=${DB_ROOT_PASSWORD}" >> /etc/mysql/mariadb.cnf
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
+
 service mysql start
-#sleep 5
 mysql -u root --skip-password -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
 mysql -u root --skip-password -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%';"
 mysql -u root --skip-password -e "UPDATE mysql.user set plugin='mysql_native_password' WHERE user = 'root' ;"
-#echo "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE} " | mariadb
-#echo "CREATE USER 'wordpress'@'%' IDENTIFIED BY '${DB_PASSWORD}';" | mariadb
-#echo "GRANT ALL ON ${DB_DATABASE}.* TO '${DB_USER}'@'%';" | mariadb
-###echo "UPDATE mysql.user set plugin='mysql_native_password' WHERE user = 'root' ;" | mariadb
-###echo "UPDATE mysql.user set plugin='mysql_native_password' WHERE user = '${DB_USER}' ;" | mariadb
-#echo "FLUSH PRIVILEGES;" | mariadb
 mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
-###echo "ALTER USER '${DB_USER}' IDENTIFIED BY '${DB_PASSWORD}';" | mariadb
 service mysql stop
 mysqld
